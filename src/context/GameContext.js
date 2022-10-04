5d7d07e4c4947a821df12f7f3b6b99473487372d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-10-04 14:24:34 -0500

    Create a handleBoxClick function that updates box content and message on click

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -9,11 +9,22 @@ export function GameContextProvider({ children }) {
 
   const [board, setBoard] = useState(initialState);
   const [currentPlayer, setCurrentPlayer] = useState('X');
-  const [gameMessage, setGameMessage] = useState(`Your Turn ${currentPlayer}`);
+  const [gameMessage, setGameMessage] = useState('Your Turn X');
   const [active, setActive] = useState(true);
 
+  const handleBoxClick = (num) => {
+    if (!active) return;
+    if (board[num].content !== '') return;
+
+    setBoard((prev) =>
+      prev.map((box) => (box.position === num ? { position: num, content: currentPlayer } : box))
+    );
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    setGameMessage(currentPlayer === 'X' ? 'Your Turn O' : 'Your Turn X');
+  };
+
   return (
-    <GameContext.Provider value={{ board, currentPlayer, gameMessage, active }}>
+    <GameContext.Provider value={{ board, currentPlayer, gameMessage, active, handleBoxClick }}>
       {children}
     </GameContext.Provider>
   );
