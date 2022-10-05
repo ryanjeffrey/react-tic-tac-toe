d50f11b953d20cfd002541e17e51a4134bce793f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-10-04 19:51:35 -0500

    Add game logic for connecting three in a row, and update game message if winner

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -23,6 +23,78 @@ export function GameContextProvider({ children }) {
     setGameMessage(currentPlayer === 'X' ? 'Your Turn, O' : 'Your Turn, X');
   };
 
+  const checkWinner = () => {
+    if (
+      board[0].content !== '' &&
+      board[0].content === board[1].content &&
+      board[1].content === board[2].content
+    ) {
+      return board[2].content;
+    } else if (
+      board[3].content !== '' &&
+      board[3].content === board[4].content &&
+      board[4].content === board[5].content
+    ) {
+      return board[5].content;
+    } else if (
+      board[6].content !== '' &&
+      board[6].content === board[7].content &&
+      board[7].content === board[8].content
+    ) {
+      return board[8].content;
+    } else if (
+      board[0].content !== '' &&
+      board[0].content === board[3].content &&
+      board[3].content === board[6].content
+    ) {
+      return board[6].content;
+    } else if (
+      board[1].content !== '' &&
+      board[1].content === board[4].content &&
+      board[4].content === board[7].content
+    ) {
+      return board[7].content;
+    } else if (
+      board[2].content !== '' &&
+      board[2].content === board[5].content &&
+      board[5].content === board[8].content
+    ) {
+      return board[8].content;
+    } else if (
+      board[0].content !== '' &&
+      board[0].content === board[4].content &&
+      board[4].content === board[8].content
+    ) {
+      return board[8].content;
+    } else if (
+      board[2].content !== '' &&
+      board[2].content === board[4].content &&
+      board[4].content === board[6].content
+    ) {
+      return board[6].content;
+    } else {
+      return false;
+    }
+  };
+
+  const isCatsGame = () => {
+    return board.filter((box) => box.content === '').length === 0;
+  };
+
+  const checkGameStatus = () => {
+    if (!active) return;
+    const winner = checkWinner();
+    if (winner) {
+      setGameMessage(`🏆 You win, ${winner}! 🏆`);
+      setActive(false);
+    } else if (isCatsGame()) {
+      setGameMessage('Cats Game!');
+      setActive(false);
+    }
+  };
+
+  checkGameStatus();
+
   return (
     <GameContext.Provider value={{ board, currentPlayer, gameMessage, active, handleBoxClick }}>
       {children}
